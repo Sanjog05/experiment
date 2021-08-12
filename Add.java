08ff4bb0d1046a3de9a9cfe773ae7d9512f7b711
Sanjog05 <88317350+Sanjog05@users.noreply.github.com>
2021-08-12 17:00:40 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add {
+
+   public static void main(String[] args) {
+        
+      int num1 = 120, num2 = 25, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
